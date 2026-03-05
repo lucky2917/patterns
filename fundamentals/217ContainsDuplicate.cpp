6c20c0a81ae57b4c8005482a82af2a268ef47c36
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-03-05 17:05:25 +0530

    217. Contains Duplicate

---
+++ fundamentals/217ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+//lc 217
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(int i = 0; i< nums.size();i++){
+            if(st.count(nums[i])) return true;
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
