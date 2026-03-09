068f00d644e1095ef2acc083bef84fe7304d3605
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-03-09 19:03:05 +0530

    80. Remove Duplicates from Sorted Array II

---
+++ two-pointers/80RemoveDuplicatesfromSortedArrayII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+        if(nums.size() <=2 ) return nums.size();
+        for(int i =2; i<nums.size();i++){
+            if(nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
