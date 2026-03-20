cb5119ad0acc883e7217a8a2d6209f2d6b4c43e2
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-03-20 23:33:42 +0530

    53. Maximum Subarray

---
+++ kadane's-Algotithm/53MaximumSubarray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = 0;
+        int maxSum = INT_MIN;
+        for(int i=0; i < nums.size(); i++){
+            currentSum = nums[i] + currentSum;
+            maxSum = max(currentSum , maxSum);
+            if(currentSum<0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
