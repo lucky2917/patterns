4c36c5f800d70c86726960caf719536f08fe2ab0
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-04-12 19:24:39 +0530

    141. Linked List Cycle

---
+++ LinkedLists/141LinkedListCycle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* curr = head;
+        ListNode* slow = curr;
+        ListNode* fast = curr;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
