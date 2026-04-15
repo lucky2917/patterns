717711160518b126c5095975881454da02105a50
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-04-15 21:19:51 +0530

    post-order traversal for binary trees

---
+++ trees/post-order.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Node
+{
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+class Solution {
+  public:
+    void helper(Node* root, vector<int>& result){
+        if(root == NULL) return;
+        helper(root->left,result);
+        helper(root->right,result);
+        result.push_back(root->data);
+    }
+    vector<int> postOrder(Node* root) {
+        // code here
+        vector<int> result;
+        helper(root, result);
+        return result;
+    }
+};
