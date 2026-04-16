076d4b3538dd8946f91608c2c6c661ef458d548f
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-04-16 22:10:41 +0530

    maximum depth of binary tree

---
+++ trees/104MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class TreeNode
+{
+  public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1+ max(left,right);
+    }
+};
