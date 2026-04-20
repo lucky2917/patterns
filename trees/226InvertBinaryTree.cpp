e0977c462ecf2bd4b1be0c56c24fec821dc4e62e
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-04-20 19:53:20 +0530

    invert binary tree function

---
+++ trees/226InvertBinaryTree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class TreeNode
+{
+  public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            TreeNode* str = node->left;
+            node->left = node->right;
+            node->right = str;
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+            q.pop();
+        }
+        return root;
+    }
+};
