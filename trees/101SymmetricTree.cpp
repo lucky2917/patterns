8e45730d749dc3f2b701fd0011a800b1e3af240c
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-04-22 23:16:15 +0530

    solution to check if a binary tree is symmetric

---
+++ trees/101SymmetricTree.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class TreeNode
+{
+  public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        val = x;
+        left = right = NULL;
+    }
+};
+class Solution {
+public:
+    bool isMirror(TreeNode* n1, TreeNode* n2){
+        if(n1 == NULL && n2 == NULL) return true;
+        if(n1 == NULL || n2 == NULL) return false;
+        return (n1->val == n2->val) && isMirror(n1->left,n2->right) && isMirror(n1->right,n2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL) return true;
+        return isMirror(root->left, root->right);
+    }
+};
