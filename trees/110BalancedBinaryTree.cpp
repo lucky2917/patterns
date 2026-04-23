22c4b115f1cf7cd1cda53755470728b8934e0f68
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-04-23 11:26:34 +0530

    solution to check if a binary tree is balanced

---
+++ trees/110BalancedBinaryTree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class TreeNode
+{
+  public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if(root == NULL) return 0;
+        int left = height(root->left);
+        if(left == -1) return -1;
+        int right = height(root->right);
+        if(right == -1) return -1;
+        if(abs(left-right)>1) return -1;
+        return 1+max(left,right);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root)!=-1;
+    }
+};
