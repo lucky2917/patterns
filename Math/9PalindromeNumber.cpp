abf48dd6bfa87e0de2645e3b6be43ded657cb72f
Gandreddi Ravi Sankar <arjun.gandreddi2005@gmail.com>
2026-05-18 23:58:03 +0530

    9. Palindrome Number

---
+++ Math/9PalindromeNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+     bool isPalindrome(int x) {
+
+        if(x<0) {
+            return false;
+        }
+        int original=x;
+        long long rev=0;
+
+        while(x > 0) {
+
+            int digit = x%10;
+
+            rev = rev *10+ digit;
+
+            x /=10;
+        }
+
+        return rev==original;
+    }
+};
